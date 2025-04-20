bba5a0d9bcc49dcda610761bc59d22a725d422d2
Farid-Jam <farid.jamshid77@gmail.com>
2025-04-20 18:33:27 -0400

    twoSum solution

---
+++ Arrays&Hashing/twoSum.cpp
@@ -0,0 +1,22 @@
+// Given an array of integers nums and an integer target, return the indices i and j 
+// such that nums[i] + nums[j] == target and i != j.
+
+// You may assume that every input has exactly one pair of indices i and j that 
+// satisfy the condition.
+
+// Return the answer with the smaller index first.
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, int> index;
+            for (int i = 0; i < nums.size(); i++){
+                int diff = target-nums[i];
+                if (index.count(x)){
+                    return {index[diff], i};
+                }
+                index.insert({nums[diff], i});
+            }
+        }
+    };
+    
